c04d55a166fdfc09f70b5454c3c26d667a0d5eb1
Matheus Viacava Sigoli <75397672+Matheus-hub-lab@users.noreply.github.com>
2020-12-21 16:39:14 -0300

    Add files via upload

---
+++ ex115/lib/arquivo/__init__.py
@@ -0,0 +1,53 @@
+
+from ex115.lib.interface import *
+
+def arquivoExiste(nome):
+    try:
+        a = open(nome,'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+
+def criaArquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print('Houve um ERRO na criação do arquivo')
+    else:
+        print(f'Arquivo {nome} criado com sucesso!')
+
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Erro ao ler o arquivo!')
+    else:
+        cabecalho('PESSOAS CADASTRADAS')
+        print(a.read())
+
+    finally:
+        a.close()
+
+
+def cadastrar(arq, nome='Desconhecido', idade=0):
+    try:
+        a=open(arq, 'at')
+    except:
+        print('houve um ERRO na abertura do arquivo!')
+    else:
+        try:
+            a.write(f'{nome},{idade}\n')
+        except:
+            print('Houve um ERRO na hora de escrever os dados!')
+        else:
+            print(f'Novo registro de {nome} adicionado')
+            a.close()
+
+
+
+
---
+++ ex115/sistema.py
@@ -0,0 +1,24 @@
+from ex115.lib.interface import *
+from ex115.lib.arquivo import *
+from time import sleep
+
+arq = 'arquivo.txt'
+
+if not arquivoExiste(arq):
+    criaArquivo(arq)
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        lerArquivo(arq)
+    elif resposta == 2:
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        cabecalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    #sleep(2)
